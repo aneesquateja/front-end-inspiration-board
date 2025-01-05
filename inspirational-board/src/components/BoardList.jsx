86c6815c6c95befed29c1972fd257c0fe1f3aad9
Anees Quateja <aneesq87@gmail.com>
2025-01-05 11:57:24 -0800

    getting started

---
+++ inspirational-board/src/components/BoardList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Board from "./Board";
+
+
+const BoardList = ({ boards }) => {
+    return (
+        <div>
+            <h1>Boards</h1>
+            <ul>
+                {boards.map((board) => (
+                    <li key={board.id}>{board.title}</li>
+                ))} 
+            </ul>
+        </div>
+    );
+};
+
+BoardList.prototype = {
+    boards: PropTypes.array.isRequired,
+};
+
+export default BoardList;
