37af32f31d863df7c67a59eb684427d7b35dc9ee
Anees Quateja <aneesq87@gmail.com>
2025-01-07 15:17:13 -0800

    removed all commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,3 @@
-// import React, { useState } from 'react';
-// import NewBoardForm from './components/NewBoardForm';
-// import './App.css';
-
-// const App = () => {
-//     const [boards, setBoards] = useState([]);
-
-//     const handleAddBoard = (newBoard) => {
-//         const boardWithId = { ...newBoard, id: boards.length + 1 };
-//         setBoards([...boards, boardWithId]);
-//     };
-
-//     return (
-//         <div className="App">
-//             <h1>Inspiration Board</h1>
-//             {/* New Board Form */}
-//             <NewBoardForm onAddBoard={handleAddBoard} />
-
-//             {/* Display Added Boards */}
-//             <div className="boards-list">
-//                 <h2>Boards</h2>
-//                 <ul>
-//                     {boards.map((board) => (
-//                         <li key={board.id}>
-//                             <strong>{board.title}</strong> by {board.owner}
-//                         </li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default App;
 
 import { useState, useEffect } from "react";
 import axios from "axios";
